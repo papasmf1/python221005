aeda1235c28e41c7effc3b16f8c60f65f3284412
papasmf1 <papasmf1@gmail.com>
2022-10-07 13:44:39 +0900

    네이버 웹툰 크롤링

--- web1.py
+++ web1.py
@@ -9,6 +9,19 @@ page = open("c:\\work\\test01.html", "rt",
 soup = BeautifulSoup(page, "html.parser")
 #print( soup.prettify() )
 #<p>전체를 검색
-print( soup.find_all("p") )
+#print( soup.find_all("p") )
+#첫번째<p>태그 
+#print( soup.find("p") )
+#id=first 
+#print( soup.find_all(id="first") )
+#조건: <p class='outer-text'>컨텐츠</p>
+#print( soup.find_all("p", class_="outer-text") )
+
+#결국은 컨텐츠를 가져오기 
+for tag in soup.find_all("p"):
+    content = tag.text.strip()
+    content = content.replace("\n", "")
+    print(content)
+
 
 
---
+++ web2.py
@@ -0,0 +1,21 @@
+# web2.py
+#웹서버에 요청 
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
+
+for i in range(1,6):
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" \
+        + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    soup = BeautifulSoup(data, "html.parser")
+    cartoons = soup.find_all("td", class_="title")	
+    # <td class="title">
+    # 	<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+    # </td>
+    for item in cartoons:
+        title = item.text.strip()
+        print(title)
+
+                

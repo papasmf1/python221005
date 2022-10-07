c894f70d62b3248771dd3735b1f6ff90c66276e7
papasmf1 <papasmf1@gmail.com>
2022-10-07 11:50:36 +0900

    웹 크롤링 연습1

---
+++ web1.py
@@ -0,0 +1,14 @@
+# web1.py 
+from bs4 import BeautifulSoup
+
+#문자열로 로딩
+page = open("c:\\work\\test01.html", "rt", 
+    encoding="utf-8").read() 
+
+#검색이 용이한 스프 객체
+soup = BeautifulSoup(page, "html.parser")
+#print( soup.prettify() )
+#<p>전체를 검색
+print( soup.find_all("p") )
+
+
